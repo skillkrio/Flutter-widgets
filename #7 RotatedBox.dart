acbc9a902fdc484502e290becd41e50c77663ab8
skillkrio <skillkrio@gmail.com>
2021-09-30 00:11:20 +0530

    Add files via upload

---
+++ #7 RotatedBox.dart
@@ -0,0 +1,33 @@
+import "package:flutter/material.dart";
+
+class MyApp extends StatelessWidget {
+  final String title = "RoatedBox";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.red,
+        primaryColorLight: Colors.orange,
+        accentColor: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: RotatedBox(
+            //
+            quarterTurns: 2,
+            //0-straight,1-right,2-down,3-left,4-straight and continues on this cycle
+            child: Image(
+              image: AssetImage("assets/tweety.png"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() => runApp(MyApp());
